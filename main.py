fce70da683d7a1047f48575eb74694b52c226471
Alex Tavgen <tavgen.alexey@gmail.com>
2018-05-08 22:48:53 +0300

    Ingest test with skafos engine

--- main.py
+++ main.py
@@ -5,25 +5,27 @@ from helpers.logger import get_logger
 from skafossdk import *
 from twitch.twitch_processor import TwitchProcessor
 
-if 'CLIENT_ID' in os.environ:
-    api_key = os.environ['CLIENT_ID']
-else:
-    sys.exit('Please save a Twitch api key in your environment.')
 
-if 'BATCH_SIZE' in os.environ:
-    n = int(os.environ['BATCH_SIZE'])
-else:
-    n = 10
+def get_twitch():
+    if 'CLIENT_ID' in os.environ:
+        api_key = os.environ['CLIENT_ID']
+    else:
+        api_key = 'rzyu2ma6hklfiw6i1yjvq0oiqhop0v'
+        # sys.exit('Please save a Twitch api key in your environment.')
 
-if 'VIEW_COUNT' in os.environ:
-    cnt = int(os.environ['VIEW_COUNT'])
-else:
-    cnt = 500
+    if 'VIEW_COUNT' in os.environ:
+        cnt = int(os.environ['VIEW_COUNT'])
+    else:
+        cnt = 500
 
-# Initialize the skafos sdk
-ska = Skafos()
+    # Initialize the skafos sdk
+    ska = Skafos()
 
-ingest_log = get_logger('user-fetch')
-processor = TwitchProcessor(api_key, ingest_log, min_viewer_count=cnt).fetch()
-ti = SocialStatements(processor.info)
-ti.save(ska)
+    ingest_log = get_logger('user-fetch')
+    processor = TwitchProcessor(api_key, ingest_log, min_viewer_count=cnt).fetch()
+    ti = SocialStatements(processor.info)
+    ti.save(ska)
+
+
+if __name__ == "__main__":
+    get_twitch()
