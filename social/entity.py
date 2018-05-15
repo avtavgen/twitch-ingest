61c95d2a39e6d4197ce8407f64e5cd0c32269239
alex tavgen <tavgen.alexey@gmail.com>
2018-05-15 16:55:08 -0700

    test skafos

--- social/entity.py
+++ social/entity.py
@@ -36,13 +36,13 @@ class SocialStatements:
         self.users = users
         if self.users:
             logger.info('about to send {} user statements to the data engine'.format(len(self.users)))
-            self._write_batches(self.engine, logger, self.user_schema, self.users, batch_size)
+            self._write_batches(self.engine.engine, logger, self.user_schema, self.users, batch_size)
         else:
             logger.debug('skipping user ingest, no records in these social statements')
 
     @staticmethod
     def _write_batches(engine, logger, schema, data, batch_size=40):
         for rows in batches(data, batch_size):
-            # logger.info('Rows: {}'.format(rows))
+            logger.info('Rows: {}'.format(rows))
             res = engine.save(schema, list(rows)).result()
             logger.info(res)
